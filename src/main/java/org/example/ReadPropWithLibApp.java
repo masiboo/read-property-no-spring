256c7a8e752587288971e7a7f5eefacb44ffd434
Masum Islam <masum.islam@dcos.org>
2022-03-13 09:29:37 +0100

    Rename class

--- src/main/java/org/example/ReadPropWithLibApp.java
+++ src/main/java/org/example/ReadPropWithLibApp.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import java.io.IOException;
-
-public class ReadProfWithLibApp {
+public class ReadPropWithLibApp {
 
     public static void main(String[] args) {
 
--- src/main/java/org/example/ReadPropWithoutLibApp.java
+++ src/main/java/org/example/ReadPropWithoutLibApp.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class ReadProfWithoutLibApp {
+public class ReadPropWithoutLibApp {
 
     public static void main(String[] args) {
 
